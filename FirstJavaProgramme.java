d54250994373c283223c1ef935d6eea232685d19
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-02-25 14:15:55 +0530

    first commit by train

--- FirstJavaProgramme.java
+++ FirstJavaProgramme.java
@@ -1,5 +1,5 @@
 class FirstJavaProgramme{
 public static void main(String str[]) {
-	System.out.println("Starting training programme with capgemini");
+	System.out.println("Starting training programme with capgemini company");
 	}
 }
